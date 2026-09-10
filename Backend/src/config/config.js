49fd8b120a9ac71deab0ac0ea7a43918df34b8e5
lakshay <lakshaygupta580@gmail.com>
2026-09-10 15:54:57 +0530

    Fix Google OAuth callback for production

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -37,11 +37,16 @@ if(!process.env.RAZORPAY_KEY_SECRET){
     throw new Error("RAZORPAY_KEY_SECRET is not defined in environment variable")
 }
 
+if(!process.env.GOOGLE_CALLBACK_URL){
+    throw new Error("GOOGLE_CALLBACK_URL is not defined in environment variable")
+}
+
 export const config = {
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_CALLBACK_URL:process.env.GOOGLE_CALLBACK_URL,
     NODE_ENV: process.env.NODE_ENV || "development",
     IMAGEKIT_PRIVATE_KEY:process.env.IMAGEKIT_PRIVATE_KEY,
     IMAGEKIT_PUBLIC_KEY:process.env.IMAGEKIT_PUBLIC_KEY,
